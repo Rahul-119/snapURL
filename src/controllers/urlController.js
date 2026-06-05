a853217add8cca52fe18f273a3b7ae78f0885a68
Rahul Dutta <rahulstudy119@gmail.com>
2026-06-06 04:18:37 +0530

    added collision handling

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,12 +1,89 @@
-import router from '../routes/urlRouter';
-import express from 'express';
+import pool from '../db/index.js';
 
-const app = express();
+async function shortenURL(req, res) {
+    try {
+        let url = req.body.url;
+        if (!url) {
+            res.status(400).json({ error: "URL is required" });
+            return;
+        }
+        url = url.trim();
 
-try {
-    const url = req.body.url;
-    console.log(url);
+        if(URL.canParse(url)) {
+            const shortCode = await generateUniqueCode();
+            await pool.query("INSERT INTO urls (long_url, short_code) VALUES (?, ?)", [url, shortCode])
+            res.json({
+                shortUrl: `http://localhost:3000/${shortCode}`
+            });
+            return;
+        }
+        else {
+            res.status(400).json({error: "Invalid URL"});
+            return;
+        }
     
-} catch (error) {
-    
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error: "Server Error"})
+    }
 }
+
+async function redirectURL(req, res) {
+    try {
+        let code = req.params.code;
+
+        const [rows] = await pool.query("SELECT long_url FROM urls WHERE short_code = ?", [code]);
+
+        if(rows.length === 0) {
+            res.status(404).json({error: "URL not found"})
+            return;
+        }
+
+        await pool.query("UPDATE urls SET clicks = clicks + 1 WHERE short_code = ?", [code])
+        return res.redirect(rows[0].long_url);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error: "Database Error"})
+    }
+}
+
+async function getStats(req, res) {
+    try {
+        const code = req.params.code;
+
+        const [rows] = await pool.query(
+            "SELECT short_code, clicks FROM urls WHERE short_code = ?",
+            [code]
+        );
+
+        if (rows.length === 0) {
+            return res.status(404).json({
+                error: "URL not found"
+            });
+        }
+
+        return res.json({
+            shortCode: rows[0].short_code,
+            clicks: rows[0].clicks
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            error: "Database Error"
+        });
+    }
+}
+
+async function generateUniqueCode() {
+    while(true) {
+        const code = Math.random().toString(36).substring(2, 8);
+
+        const [rows] = await pool.query("SELECT id FROM urls WHERE short_code = ?", [code]);
+
+        if(rows.length === 0) {
+            return code;
+        }
+    }
+}
+export {shortenURL, redirectURL, getStats}
--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,50 +1,14 @@
 import express from 'express';
-import pool from '../db/index.js';
+import { shortenURL, redirectURL, getStats } from '../controllers/urlController.js';
 const router = express.Router();
 
-router.post('/shorten', async (req, res) => {
-    try {
-        let url = req.body.url;
-        if (!url) {
-            res.status(400).json({ error: "URL is required" });
-            return;
-        }
-        url = url.trim();
-
-        if(URL.canParse(url)) {
-            const shortCode = Math.random().toString(36).substring(2, 8);
-            await pool.query("INSERT INTO urls (long_url, short_code) VALUES (?, ?)", [url, shortCode])
-            res.json({
-                shortUrl: `http://localhost:3000/${shortCode}`
-            });
-            return;
-        }
-        else {
-            res.status(400).json({error: "Invalid URL"});
-            return;
-        }
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({error: "Server Error"})
-    }
+router.post('/shorten', (req, res) => {
+    shortenURL(req, res);
 })
 
-router.get('/:code', async (req, res) => {
-    try {
-        console.log(req.params);
-        
-        let code = req.params.code;
-
-        const [rows] = await pool.query("SELECT long_url FROM urls WHERE short_code = ?", [code]);
-
-        if(rows.length === 0) {
-            res.status(404).json({error: "URL not found"})
-            return;
-        }
-        return res.redirect(rows[0].long_url);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({error: "Database Error"})
-    }
+router.get('/stats/:code',getStats);
+router.get('/:code', (req, res) => {
+    redirectURL(req, res);
 })
+
 export default router;
